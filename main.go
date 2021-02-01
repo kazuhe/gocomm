b6e1fdeb48ee31902cab69cd899097fd633439f3
Kazuhiro Ohara <oohara.k@tam-tam.co.jp>
2021-02-01 23:59:13 +0900

    add: 自己署名SSL証明書とサーバの秘密鍵の生成

--- main.go
+++ main.go
@@ -14,9 +14,14 @@ func main() {
 	mux.HandleFunc("/", index)
 
 	server := &http.Server{
-		Addr:    "0.0.0.0:8080",
+		Addr:    "127.0.0.1:8080",
 		Handler: mux,
 	}
-	server.ListenAndServe()
+
+	gencert()
+
+	// HTTPSで運用するには'ListenAndServeTLS'を使用
+	// cert.pem: SSL証明書, key.pem: サーバ用の秘密鍵
+	server.ListenAndServeTLS("cert.pem", "key.pem")
 
 }
