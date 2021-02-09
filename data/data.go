fbf30e69d8b6d873226cd5f86587ad5f30649e51
kazuhe <omc.ohara@gmail.com>
2021-02-09 11:32:10 +0900

    add: Postに関係付けられたCommentsテーブルとそれを保存/取得する記述

--- data/data.go
+++ data/data.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 
 	// PostgreSQLのデータベースドライバ
@@ -10,9 +11,18 @@ import (
 
 // Post ユーザーの投稿を表す構造体
 type Post struct {
+	ID       int
+	Content  string
+	Author   string
+	Comments []Comment
+}
+
+// Comment Postに対するユーザーのコメントを表す構造体
+type Comment struct {
 	ID      int
 	Content string
 	Author  string
+	Post    *Post
 }
 
 // DB データベースへのハンドルであり、データベース接続のプールを表す
@@ -26,6 +36,15 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func (comment *Comment) Create() (err error) {
+	if comment.Post == nil {
+		err = errors.New("投稿が見つかりません")
+		return
+	}
+	err = DB.QueryRow("insert into comments (content, author, post_id) values ($1, $2, $3) returning id", comment.Content, comment.Author, comment.Post.ID).Scan(&comment.ID)
+	return
 }
 
 // Posts 最新の投稿からパラメータで受け取ったn個の投稿を取得する
@@ -48,11 +67,28 @@ func Posts(limit int) (posts []Post, err error) {
 	return
 }
 
-// GetPostByID パラメータIDによって1件のPostを取得
+// GetPostByID パラメータIDによってPost.Commentsをセットした後に1件のPostを取得
 func GetPostByID(id int) (post Post, err error) {
 	post = Post{}
+	post.Comments = []Comment{}
 	// SQLのselectコマンドを使って取得したデータ（id, content, author）をpostに参照渡し
 	err = DB.QueryRow("select id, content, author from posts where id = $1", id).Scan(&post.ID, &post.Content, &post.Author)
+
+	// 'Query'を使ってSQLからコメントを複数行（*sql.Rows）取得
+	rows, err := DB.Query("select id, content, author from comments where post_id = $1", id)
+	if err != nil {
+		return
+	}
+	// *sql.Rowsの'Next'メソッドを使って複数行の中から単一行を取得
+	for rows.Next() {
+		comment := Comment{Post: &post}
+		err = rows.Scan(&comment.ID, &comment.Content, &comment.Author)
+		if err != nil {
+			return
+		}
+		post.Comments = append(post.Comments, comment)
+	}
+	rows.Close()
 	return
 }
 
@@ -91,13 +127,17 @@ func (post *Post) Delete() (err error) {
 // DBConnect DBの挙動確認用
 func DBConnect() {
 	post := Post{Content: "Hello World!", Author: "kazuhe"}
-
 	fmt.Println(post)
 	post.Create()
 	fmt.Println(post)
 
+	comment := Comment{Content: "投稿に対するコメント", Author: "kazuhe", Post: &post}
+	comment.Create()
+
 	readPost, _ := GetPostByID(post.ID)
 	fmt.Println(readPost)
+	fmt.Println(readPost.Comments)
+	// fmt.Println(readPost.Comments[0].Post)
 
 	readPost.Content = "Bonjour Monde"
 	readPost.Author = "kazupi"
