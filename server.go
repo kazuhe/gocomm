fff971b3ddb6048e4f7460afcaedcd204d88b79a
kazuhe <omc.ohara@gmail.com>
2021-02-13 02:59:15 +0900

    add: 検証のためリクエスト情報の出力/コメント

--- server.go
+++ server.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"net/http/httputil"
 	"path"
 	"strconv"
 
@@ -18,11 +21,16 @@ func main() {
 	// 全てのハンドラ関数は第1引数に'ResponseWriter'をとり、
 	// 第2引数に'Request'をとるので改めて引数を渡す必要はない
 	http.HandleFunc("/post/", handleRequest)
+	log.Println("start http listenig 127.0.0.1:8080")
 	server.ListenAndServe()
 }
 
 // handleRequest リクエストを正しい関数に振り分けるためのハンドラ
 func handleRequest(w http.ResponseWriter, r *http.Request) {
+	// 検証のためにリクエストに含まれる情報を出力
+	dump, _ := httputil.DumpRequest(r, true)
+	fmt.Println(string(dump))
+
 	var err error
 
 	// リクエストメソッドに応じてそれぞれのCRUD関数に作業を振り分ける
@@ -45,19 +53,27 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// handleGet /post/<id> GETリクエストに応じて投稿を読み出す関数
 func handleGet(w http.ResponseWriter, r *http.Request) (err error) {
+	// URLのパスを抽出
 	id, err := strconv.Atoi(path.Base(r.URL.Path))
 	if err != nil {
 		return
 	}
+
+	// メソッドRetriveでidを元に構造体Postを作成
 	post, err := data.Retrive(id)
 	if err != nil {
 		return
 	}
+
+	// 構造体PostをJSONフォーマットのバイト列に変換
 	output, err := json.MarshalIndent(&post, "", "\t")
 	if err != nil {
 		return
 	}
+
+	// バイト列をResponseWriterに書き出す
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(output)
 	return
