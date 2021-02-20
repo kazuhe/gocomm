5c96f840f91f610d30c45f1c56b3aafc145e5486
kazuhe <omc.ohara@gmail.com>
2021-02-20 17:16:33 +0900

    add: PUTリクスとのテストとテスト共通処理

--- server_test.go
+++ server_test.go
@@ -4,21 +4,38 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/kazuhe/gocomm/data"
 )
 
-// TestHandleGet GETリクエストで正常にJSONを返しているかテスト
-func TestHandleGet(t *testing.T) {
+var mux *http.ServeMux
+var writer *httptest.ResponseRecorder
+
+// TestMain テストの共通処理
+func TestMain(m *testing.M) {
+	setUp()
+
+	// 個々のテストケース関数を呼び出す
+	code := m.Run()
+	os.Exit(code)
+}
+
+// setUp 全てのテストケースの前処理
+func setUp() {
 	// 各テストケースは独立して実行されるため
-	// テスト関数内でマルチプレクサを生成
-	mux := http.NewServeMux()
+	// テスト毎にマルチプレクサを生成
+	mux = http.NewServeMux()
 	// テスト対象のハンドルを付加
 	mux.HandleFunc("/post/", handleRequest)
-
 	// レスポンス受け取るHTTPレスポンスレコーダーを定義
-	writer := httptest.NewRecorder()
+	writer = httptest.NewRecorder()
+}
+
+// TestHandleGet GETリクエストで正常にJSONを返しているかテスト
+func TestHandleGet(t *testing.T) {
 	// テスト対象へのリクエストを作成
 	request, _ := http.NewRequest("GET", "/post/1", nil)
 
@@ -37,5 +54,16 @@ func TestHandleGet(t *testing.T) {
 	// JSONを取得できているかテスト
 	if post.ID != 1 {
 		t.Errorf("Cannot retrieve JSON post")
+	}
+}
+
+// TestHandlePut PUTリクエストでエラーが返されないかテスト
+func TestHandlePut(t *testing.T) {
+	json := strings.NewReader(`{"content":"Updates post","autor":"kazuhe"}`)
+	request, _ := http.NewRequest("PUT", "/post/1", json)
+	mux.ServeHTTP(writer, request)
+
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
 	}
 }
